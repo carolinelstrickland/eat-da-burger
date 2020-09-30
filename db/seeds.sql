17912acf593773340bcc2a0f07c6326b19ec9bfd
Caroline Strickland <carolinelstrickland@outlook.com>
2020-09-30 18:44:27 -0500

    add schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ("The Fuh-gouda-bout-it Burger");
+INSERT INTO burgers (name) VALUES ("The Can I Have Your Slaw-tograph Burger?");
+INSERT INTO burgers (name) VALUES ("Tunami");
+INSERT INTO burgers (name) VALUES ("Carrot On My Wayward Bun");
