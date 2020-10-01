437b4200f3da2f6c54ac801f5ca38abaf0c04a75
Caroline Strickland <carolinelstrickland@outlook.com>
2020-09-30 19:00:49 -0500

    creating config directory and connection and orm files

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers 
 (
     id int NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255), NOT NULL,
+    name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
+
